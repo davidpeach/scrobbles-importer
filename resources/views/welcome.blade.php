5ff18482d4abd2710e5c2e7733be2465b288ae81
David Peach <mail@davidpeach.co.uk>
2019-10-26 20:16:40 +0100

    Continue Testing Github Actions2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Scrobble Importer2
+                    Scrobble Importer3
                 </div>
 
                 <div class="links">
